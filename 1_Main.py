7765dc4073d9c27f8bb76c0c9d0e558d7418613f
Ezra Tan <71835370+ezratanenzhong@users.noreply.github.com>
2023-01-01 18:05:58 +0800

    Update 1_Main.py

--- 1_Main.py
+++ 1_Main.py
@@ -2,11 +2,18 @@ import streamlit as st
 
 st.title("Customer Review Analyzer")
 
-st.markdown("The analyzer can identify the sentiment label of reviews ")
+
+st.markdown("""
+    The analyzer can identify the sentiment label of customer reviews, whether it is positive, negative, or neutral.
+    **👈 Select a page from the sidebar** to see what the analyzer can do
+    - Choose 'Single Prediction' if want to predict the sentiment of a review
+    - Choose 'Batch Prediction' if want to predict the sentiment of reviews in batch   
+""")
+
 
 with st.sidebar:
     st.sidebar.success('Select a page above')
     st.subheader('About')
     st.markdown('This is a web application to identify the sentiment label of reviews provided by customers.')
-    st.markdown("Code: Github")
+    st.markdown("Code: https://github.com/ezratanenzhong/SentimentAnalysis")
 
