650587bb103050c80d2b94b2da4fd9bb3c3d8622
Ezra Tan <71835370+ezratanenzhong@users.noreply.github.com>
2023-01-04 02:36:14 +0800

    Update 1_Main.py

--- 1_Main.py
+++ 1_Main.py
@@ -7,11 +7,12 @@ st.markdown("""
     - Choose 'Single Prediction' if want to predict the sentiment of a review
     - Choose 'Batch Prediction' if want to predict the sentiment of reviews in batch   
 """)
-st.image("Image 1.png")
+st.image("Image 1.jpg")
 
 with st.sidebar:
     st.sidebar.success('Select a page above')
     st.subheader('About')
     st.markdown('This is a web application to identify the sentiment label of reviews provided by customers.')
     st.markdown("Code: https://github.com/ezratanenzhong/SentimentAnalysis")
+    st.image("Image 3.jpg")
 
