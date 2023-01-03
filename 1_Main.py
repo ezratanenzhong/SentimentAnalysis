43dc3c36ec7a8601c7f87e7b5073f3d9e3000139
Ezra Tan <71835370+ezratanenzhong@users.noreply.github.com>
2023-01-04 02:34:35 +0800

    Update 1_Main.py

--- 1_Main.py
+++ 1_Main.py
@@ -1,15 +1,13 @@
 import streamlit as st
 
 st.title("Customer Review Analyzer")
-
-
 st.markdown("""
     The analyzer can identify the sentiment label of customer reviews, whether it is positive, negative, or neutral.
     **👈 Select a page from the sidebar** to see what the analyzer can do
     - Choose 'Single Prediction' if want to predict the sentiment of a review
     - Choose 'Batch Prediction' if want to predict the sentiment of reviews in batch   
 """)
-
+st.image("Image 1.png")
 
 with st.sidebar:
     st.sidebar.success('Select a page above')
