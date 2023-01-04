9277289404cb04d6626d92faae2fdb4313bc4c95
Ezra Tan <71835370+ezratanenzhong@users.noreply.github.com>
2023-01-05 01:21:06 +0800

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -8,8 +8,8 @@ col1, col2 = st.columns(2)
 with col1:
     st.markdown("""
         Welcome to Reviewalyze ! The web application is designed to **analyze reviews** and help businesses and individuals **realize** the sentiment of reviews on them.
-        The Sentiment Analyzer can be useful for businesses looking to improve customer satisfaction or for individuals looking to understand the sentiment of a brand or product."  
-    """)
+        The Sentiment Analyzer can be useful for businesses looking to improve customer satisfaction or for individuals looking to understand the sentiment of a brand or product.
+        """)
     st.write("**👈 Select a page from the sidebar** to try it out and get a better understanding of your customer reviews!
         - Choose 'Single Review Analysis' if want to predict the sentiment of a review
         - Choose 'Batch Review Analysis' if want to predict the sentiment of reviews in batch ")
