03977ddc4b5cc2841230e79017f246db1c373f4a
Ezra Tan <71835370+ezratanenzhong@users.noreply.github.com>
2023-01-05 01:31:31 +0800

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -6,16 +6,14 @@ st.title("Reviewalyze")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("""
-        Welcome to Reviewalyze ! The web application is designed to **analyze reviews** and help businesses and individuals **realize** the sentiment of reviews on them.
-        The Sentiment Analyzer can be useful for businesses looking to improve customer satisfaction or for individuals looking to understand the sentiment of a brand or product.
-        """)
-    st.write("**👈 Select a page from the sidebar** to try it out and get a better understanding of your customer reviews!")
+    st.markdown('<div style="text-align: justify;">Welcome to Reviewalyze ! The web application is designed to **analyze reviews** and help businesses and individuals **realize** the sentiment of reviews on them.</div>', unsafe_allow_html=True)
+    st.markdown('<div style="text-align: justify;">The Sentiment Analyzer can be useful for businesses looking to improve customer satisfaction or for individuals looking to understand the sentiment of a brand or product.</div>', unsafe_allow_html=True)
+    st.markdown('<div style="text-align: justify;">**👈 Select a page from the sidebar** to try it out and get a better understanding of your customer reviews!</div>', unsafe_allow_html=True)
     st.write("- Choose 'Single Review Analysis' if want to predict the sentiment of a review")
     st.write("- Choose 'Batch Review Analysis' if want to predict the sentiment of reviews in batch")
 
 with col2:
-    st.image("Image 1.jpg")
+    st.image("Image.png")
 
 with st.sidebar:
     st.sidebar.success('Select a page above')
