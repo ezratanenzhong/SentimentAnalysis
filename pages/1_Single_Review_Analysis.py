34f08a5ec04b1801191ae813607ef82af1ea6cb5
Ezra Tan <71835370+ezratanenzhong@users.noreply.github.com>
2023-01-13 20:39:42 +0800

    Update 1_Single_Review_Analysis.py

--- pages/1_Single_Review_Analysis.py
+++ pages/1_Single_Review_Analysis.py
@@ -211,7 +211,6 @@ abbreviations = {
     "pt": "point",
     "ptb": "please text back",
     "pto": "please turn over",
-    "qpsa": "what happens",  # "que pasa",
     "ratchet": "rude",
     "rbtl": "read between the lines",
     "rlrt": "real life retweet",
@@ -263,23 +262,26 @@ abbreviations = {
 
 # Remove all english stopwords
 def remove_stopwords(text):
-    text = ' '.join([word for word in text.split() if word not in stopwords.words("english")])
+    stop_words = stopwords.words("english")
+    omit_stop_words = ['not', 'nor', 'but']
+    stop_words = [el for el in stop_words if el not in omit_stop_words]
+    text = ' '.join([word for word in text.split() if word not in stop_words])
     return text
 
 # Remove all punctuations
 def remove_all_punct(text):
-    table = str.maketrans('', '', string.punctuation)
+    table = str.maketrans('','',string.punctuation)
     return text.translate(table)
 
 # Remove all URLs, replace by URL
 def remove_URL(text):
     url = re.compile(r'https?://\S+|www\.\S+')
-    return url.sub(r'URL', str(text))
+    return url.sub(r'URL',str(text))
 
 # Remove HTML beacon
 def remove_HTML(text):
-    html = re.compile(r'<.*?>')
-    return html.sub(r'', text)
+    html=re.compile(r'<.*?>')
+    return html.sub(r'',text)
 
 # Remove non printable characters
 def remove_not_ASCII(text):
@@ -299,31 +301,31 @@ def replace_abbrev(text):
 
 # Remove @ and mention, replace by USER
 def remove_mention(text):
-    at = re.compile(r'@\S+')
-    return at.sub(r'USER', text)
+    at=re.compile(r'@\S+')
+    return at.sub(r'USER',text)
 
-# Remove numbers, replace it by NUMBER
+# Remove numbers
 def remove_number(text):
-    num = re.compile(r'[-+]?[.\d]*[\d]+[:,.\d]*')
-    return num.sub(r'NUMBER', text)
+    return re.sub(r'\d+', '', text)
 
 # Remove emoji
 def remove_emoji(string):
     emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               "]+", flags=re.UNICODE)
+                           u"\U0001F600-\U0001F64F" # emoticons
+                           u"\U0001F300-\U0001F5FF" # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF" # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF" # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', string)
 
 # lemmatize text (convert to root form of text)
 def lemmatization(text):
-    lm = WordNetLemmatizer()
+    lm= WordNetLemmatizer()
     text = ' '.join([lm.lemmatize(word, pos='v') for word in text.split()])
     return text
+
 
 def clean_text(text):
     # lowercase text and remove punctuation
