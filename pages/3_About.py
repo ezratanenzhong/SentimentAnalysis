aa793da32a387ccc96d63acc90ac6eb641831154
Ezra Tan <71835370+ezratanenzhong@users.noreply.github.com>
2023-01-13 22:26:11 +0800

    Update 3_About.py

--- pages/3_About.py
+++ pages/3_About.py
@@ -12,25 +12,28 @@ st.header("User Manual")
 
 st.subheader('Single Review Analysis')
 st.write('This page can analyze the sentiment of a single review. Below are the detailed steps:')
-st.write(' 1. Click Single Review Analysis page at the sidebar')
-st.write(' 2. Enter the review in the text area')
-st.write(' 3. Click the Analyze button')
-st.write(' 4. The output will show the predicted sentiment label of the review and the class probabilities for the input data points (i.e. the probability that a particular data point falls into the underlying classes).')
+st.write(' - Click Single Review Analysis page at the sidebar')
+st.image('user_manual_1.png')
+st.write(' 1. Enter the review in the text area')
+st.write(' 2. Click the **Analyze text** button')
+st.write(' 3. View the predicted sentiment label of the review') 
+st.write(' 4. View the class probabilities for the input data points (i.e. the probability that a particular data point falls into the underlying classes).')
 
 st.subheader('Batch Review Analysis')
 st.write('This page can analyze the sentiment of multiple reviews. Below are the detailed steps:')
-st.write(' 1. Click Single Review Analysis at the sidebar')
-st.markdown("""2. Upload a file containing the reviews with the required format: 
+st.write(' - Click Batch Review Analysis at the sidebar')
+st.image('user_manual_2.png')
+st.markdown("""1. Click **Browse file** button to upload a file with the required format: 
 - CSV file has a column which contains the reviews
 - Column name must be **text**
 - Reviews can only be in English""")
-st.write("3. Click **Analyze** button. ")
-st.write("4. A table with the reviews and their predicted sentiment labels will be displayed. Click **Download data as CSV** button to download the result data.")
-st.markdown("""5. At the Visualization section, choose the type of plot to view:
+st.write("2. Click **Analyze** button. ")
+st.write("3. A table with the reviews and their predicted sentiment labels will be displayed. Click **Download data as CSV** button to download the result data.")
+st.markdown("""4. At the Visualization section, choose the type of plot to view:
 - Click **Bar Chart** button to view the distribution of the sentiment labels
 - Click **Wordcloud** button to view the words contain in positive and negative sentiment sentences
-- CLick **N-grams** button to view the N-grams words in positive and negative sentiment sentences""")
-
+- CLick **N-grams** button to view the N-gram words in positive and negative sentiment sentences""")
 
 st.markdown("Any question can email me via ezratan2001@gmail.com")
+st.write("Can contact me through my LinkedIn too: [link](https://www.linkedin.com/in/ezra-tan-en-zhong/)")
 
